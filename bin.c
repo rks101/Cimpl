62723d5b828647041af9aa8a3874f88c77e5df6f
rks <3146129+rks101@users.noreply.github.com>
2021-02-04 22:05:02 +0530

    Update bin.c

--- bin.c
+++ bin.c
@@ -2,12 +2,15 @@
 
 int main()
 {
-        int binary1, binary2;
+  int binary1, binary2;
 
-        binary1 = 0100;
-        binary2 = 0111;
+  binary1 = 0100;
+  binary2 = 0111;
 
-        printf("%d \n", binary1 + binary2);
+  // Q. what is sum of binary1 and binary2? 
+  // Hint: check base 2 (binary), 8 (octal), 16 (hexadecimal) notation. 
 
-        return 0;
+  printf("%d \n", binary1 + binary2);
+
+  return 0;
 }
