ef5ea87b92bb99eb738547ce6dc7cf4f8287ba4c
rks <3146129+rks101@users.noreply.github.com>
2021-02-04 22:02:27 +0530

    Create bin.c

---
+++ bin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+        int binary1, binary2;
+
+        binary1 = 0100;
+        binary2 = 0111;
+
+        printf("%d \n", binary1 + binary2);
+
+        return 0;
+}
